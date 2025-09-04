8a8befa5d26a976be64277667395a1436177814b
hrishikesh8904 <hrishikesh.sane89@outlook.com>
2025-09-04 23:11:20 +0530

    new file:   Classes/Enums.cpp

---
+++ Classes/Enums.cpp
@@ -0,0 +1,2 @@
+// An enum is a special data type that defines a collection of constant values under a single name. Unlike primitive constants or string literals, enums are type-safe, you can’t assign just any value to a variable declared as an enum type.
+
